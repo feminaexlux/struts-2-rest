66fafd8cde7dac95f667f01eedeae94c7b5059d6
Lucas Kam <lkam@picsauditing.com>
2013-01-16 09:17:54 -0800

    - Added IntelliJ entries for .gitignore - Finally correctly configured the web.xml and struts.xml to work - Started work on the MessagesController to redirect on save. I've been less than successful on that.

--- src/main/java/com/zulutown/struts2/rest/MessagesController.java
+++ src/main/java/com/zulutown/struts2/rest/MessagesController.java
@@ -1,10 +1,17 @@
 package com.zulutown.struts2.rest;
 
 import java.util.Collection;
+
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.apache.struts2.rest.DefaultHttpHeaders;
 import org.apache.struts2.rest.HttpHeaders;
+
 import com.opensymphony.xwork2.ModelDriven;
 
+@Results({
+	@Result(name = "update", type = "redirect", location = "/messages/1/edit")
+})
 public class MessagesController implements ModelDriven<Object> {
 
 	@SuppressWarnings("unused")
@@ -14,36 +21,45 @@ public class MessagesController implements ModelDriven<Object> {
 	private Collection<Message> list;
 
 	public HttpHeaders create() {
-		System.out.println("hello");
+		System.out.println("create");
 		MessageService.save(model);
 		list = MessageService.findAll();
 		return new DefaultHttpHeaders("index").disableCaching();
 	}
 
 	public HttpHeaders destroy() {
+		System.out.println("destroy");
 		MessageService.remove(id);
 		return new DefaultHttpHeaders("index").disableCaching();
 	}
 
 	public HttpHeaders show() {
+		System.out.println("show");
 		return new DefaultHttpHeaders("show").disableCaching();
 	}
 
 	public HttpHeaders update() {
+		System.out.println("update");
 		MessageService.save(model);
-		return new DefaultHttpHeaders("update");
+		// redirect to edit page
+		DefaultHttpHeaders headers = new DefaultHttpHeaders("update");
+		headers.disableCaching();
+		return headers;
 	}
 
 	public HttpHeaders index() {
+		System.out.println("index");
 		list = MessageService.findAll();
 		return new DefaultHttpHeaders("index").disableCaching();
 	}
 	
 	public HttpHeaders edit() {
+		System.out.println("edit");
 		return new DefaultHttpHeaders("edit").disableCaching();
 	}
 	
 	public HttpHeaders editNew() {
+		System.out.println("editNew");
 		return new DefaultHttpHeaders("edit-new").disableCaching();
 	}
 
--- src/main/webapp/WEB-INF/content/messages_edit.jsp
+++ src/main/webapp/WEB-INF/content/messages_edit.jsp
@@ -1,12 +1,13 @@
 <%@ taglib prefix="s" uri="/struts-tags"%>
 <h2>Edit New</h2>
-<s:form action="messages" method="put">
+<s:url action="messages" var="message_url" >
+	<s:param name="id">${model.id}</s:param>
+</s:url>
+<s:form action="/messages/%{model.id}" method="post">
 	<s:hidden name="model.id" />
+	<s:hidden name="_method" value="put" />
 	<s:textfield name="model.text" label="Message" />
 	<s:textfield name="model.author" label="Author" />
 	<s:submit />
-</s:form>
-<s:form action="messages/${model.id}" method="delete">
-	<s:submit value="Delete"/>
 </s:form>
 <a href="/messages">Back</a>
